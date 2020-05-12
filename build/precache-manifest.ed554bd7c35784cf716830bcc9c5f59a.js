853e59432519e215fbb6d6d7b2d7b9b084cab40a
NiinieB <mel.betourne.14@gmail.com>
2020-05-12 11:18:47 +0200

    build

---
+++ build/precache-manifest.ed554bd7c35784cf716830bcc9c5f59a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ba86a5d40ea0df6739a586cea689c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd08d4a5da26a56da240",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "766904db3b58bfe37486",
+    "url": "/static/js/2.72350636.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72350636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd08d4a5da26a56da240",
+    "url": "/static/js/main.85163ade.chunk.js"
+  },
+  {
+    "revision": "1e211b9bb8a8d46b01a7",
+    "url": "/static/js/runtime-main.33a3048b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
